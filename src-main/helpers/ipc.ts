166890e7275975884c1373053dbeb82106590d9c
Timo Siegenthaler <timosiegenthaler@gmail.com>
2023-04-22 11:36:05 +0200

    parse ipc argumentes on both sides

--- src-main/helpers/ipc.ts
+++ src-main/helpers/ipc.ts
@@ -1,14 +1,26 @@
 import { ipcMain } from 'electron'
 import { getFileContent, saveFile, selectFile, selectFolder, selectOutputFolder } from './filePicker'
 import { preparePresentation } from '../presentation/presentation'
+import { z } from 'zod'
 
 export function registerIpc() {
     ipcMain.handle('dialog:selectFile', selectFile)
     ipcMain.handle('dialog:selectFolder', selectFolder)
     ipcMain.handle('dialog:selectOutputFolder', selectOutputFolder)
-    ipcMain.handle('file:save', (_, arg1, arg2) => saveFile(arg1, arg2))
-    ipcMain.handle('file:getContent', (_, arg1) => getFileContent(arg1))
-    ipcMain.handle('preparePresentation', (_, arg1, arg2) => preparePresentation(arg1, arg2))
+    ipcMain.handle('file:save', (_, arg1, arg2) => {
+        const filePath = z.string().parse(arg1)
+        const content = z.string().parse(arg2)
+        return saveFile(filePath, content)
+    })
+    ipcMain.handle('file:getContent', (_, arg1) => {
+        const filePath = z.string().parse(arg1)
+        return getFileContent(filePath)
+    })
+    ipcMain.handle('presentation:prepare', (_, arg1, arg2) => {
+        const content = z.string().parse(arg1)
+        const templateFolderPath = z.string().parse(arg2)
+        return preparePresentation(content, templateFolderPath)
+    })
 }
 
 export function unregisterIpc() {
@@ -17,5 +29,5 @@ export function unregisterIpc() {
     ipcMain.removeHandler('dialog:selectOutputFolder')
     ipcMain.removeHandler('file:save')
     ipcMain.removeHandler('file:getContent')
-    ipcMain.removeHandler('preparePresentation')
+    ipcMain.removeHandler('presentation:prepare')
 }
--- src/ipc/presentation.ts
+++ src/ipc/presentation.ts
@@ -1,20 +1,8 @@
 import { ipcRenderer } from 'electron'
 
 const presentation = {
-    async exportPresentation(
-        presentationFilePath: string,
-        templateFolderPath: string,
-        outputFolderPath: string
-    ): Promise<string> {
-        return await ipcRenderer.invoke(
-            'exportPresentation',
-            presentationFilePath,
-            templateFolderPath,
-            outputFolderPath
-        )
-    },
     async preparePresentation(presentationFilePath: string, templateFolderPath: string): Promise<string> {
-        return await ipcRenderer.invoke('preparePresentation', presentationFilePath, templateFolderPath)
+        return await ipcRenderer.invoke('presentation:prepare', presentationFilePath, templateFolderPath)
     },
 } as const
 
