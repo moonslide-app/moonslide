ceded6a1fab54cdba90998fcfe70e28a4e348e95
Timo Siegenthaler <timosiegenthaler@gmail.com>
2023-04-22 13:34:00 +0200

    change from webpack to vite

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PreviewSlides } from './components/PreviewSlides'
 function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
             <FilePicker />
             <CodeMirrorEditor />
             <PreviewSlides />
