87e6525ce16fa6f54e66657c940250aa6fb41b7d
Simon Schuhmacher <simon@schuhmacher.im>
2023-04-23 17:44:58 +0200

    clean forge + vite setup

--- src-main/main.ts
+++ src-main/main.ts
@@ -1,65 +1,71 @@
-import { app, BrowserWindow, protocol } from 'electron'
-import { registerIpc, unregisterIpc } from '../src-main/helpers/ipc'
-import { registerRevealProtocl, REVEAL_PROTOCOL_NAME } from '../src-main/helpers/protocol'
-import { join } from 'path'
-
-declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string
-declare const MAIN_WINDOW_VITE_NAME: string
+import { app, BrowserWindow, protocol } from 'electron';
+import path from 'path';
+import { registerIpc, unregisterIpc } from './helpers/ipc';
+import { registerRevealProtocl, REVEAL_PROTOCOL_NAME } from './helpers/protocol';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-    app.quit()
+  app.quit();
 }
 
-const createWindow = (): void => {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        height: 800,
-        width: 1000,
-        webPreferences: {
-            preload: join(__dirname, 'preload.js'),
-        },
-    })
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-    registerIpc()
-    registerRevealProtocl()
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
 
-    // and load the index.html of the app.
-    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
-    } else {
-        mainWindow.loadFile(join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
-    }
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
-}
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
 
 protocol.registerSchemesAsPrivileged([{ scheme: REVEAL_PROTOCOL_NAME, privileges: { bypassCSP: true } }])
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// app.on('ready', createWindow);
+
+app.whenReady().then(() => {
+  registerIpc()
+  registerRevealProtocl()
+
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
-    unregisterIpc()
+app.on('quit', () => {
+  // unregisterIpc()
 })
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,6 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({
-    root: './src',
-})
+export default defineConfig({});
