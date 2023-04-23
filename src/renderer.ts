391336b4a6f5dae7ecd3bd5a1944eca830f99f07
Timo Siegenthaler <timosiegenthaler@gmail.com>
2023-04-23 17:47:04 +0200

    source format

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,6 @@
  * ```
  */
 
-
 import './main'
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+console.log('👋 This message is being logged by "renderer.ts", included via Vite')
