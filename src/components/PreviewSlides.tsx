358018031ed451fed72421ac8c6a5d640dd3bdba
Timo Siegenthaler <timosiegenthaler@gmail.com>
2023-05-08 16:26:15 +0200

    remove max-width contraint

--- src/components/PreviewSlides.tsx
+++ src/components/PreviewSlides.tsx
@@ -7,7 +7,11 @@ export function PreviewSlides() {
         <div className="space-y-4 max-h-[600px] overflow-y-auto">
             {slidesLastUpdate.map((update, idx) => (
                 <div key={idx}>
-                    <iframe src={`reveal://preview-small/#/${idx}`} className="w-full h-48" key={update}></iframe>
+                    <iframe
+                        src={`reveal://preview-small/#/${idx}`}
+                        className="w-full h-48 2xl:h-60"
+                        key={update}
+                    ></iframe>
                 </div>
             ))}
         </div>
