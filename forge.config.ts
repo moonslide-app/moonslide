85112dbf1904c08b90eedfa3aea7372e2fe7d19b
Simon Schuhmacher <91273944+simonschuhmacher@users.noreply.github.com>
2023-05-15 12:25:12 +0200

    add makerZIP for macos

--- forge.config.ts
+++ forge.config.ts
@@ -2,6 +2,7 @@ import { ForgeConfig } from '@electron-forge/shared-types'
 import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { MakerWix } from '@electron-forge/maker-wix'
 import { MakerDMG } from '@electron-forge/maker-dmg'
+import { MakerZIP } from '@electron-forge/maker-zip'
 import { MakerDeb } from '@electron-forge/maker-deb'
 import { MakerRpm } from '@electron-forge/maker-rpm'
 import { VitePlugin } from '@electron-forge/plugin-vite'
@@ -49,6 +50,7 @@ const config: ForgeConfig = {
             version: normalizeWindowsVersion(packageJSON.version),
         }),
         new MakerDMG({}),
+        new MakerZIP({}, ['darwin']),
         new MakerRpm({}),
         new MakerDeb({}),
     ],
