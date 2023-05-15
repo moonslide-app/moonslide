212e3f1b4641aa6d927db11d545d77a443e84a26
Timo Siegenthaler <timosiegenthaler@gmail.com>
2023-05-15 11:54:31 +0200

    remove unneeded components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { CodeMirrorEditor } from './components/CodeMirrorEditor'
-import { ExportButton } from './components/ExportButton'
 import { PreviewSlides } from './components/PreviewSlides'
 import { MenuCallbacks } from './components/MenuCallbacks'
+import { useEditorStore } from './store'
 
 function App() {
+    const editingFilePath = useEditorStore(state => state.editingFilePath)
+
     return (
         <div className="p-8 md:p-12 m-auto">
             <MenuCallbacks />
-            <h1 className="text-4xl font-semibold mb-8">Reveal Editor</h1>
-            <ExportButton />
+            <h1 className="text-4xl font-semibold mb-4">Reveal Editor</h1>
+            <p className="text-sm font-medium">Editing File: {editingFilePath}</p>
             <div className="grid grid-cols-3 2xl:grid-cols-4 gap-8">
-                <CodeMirrorEditor className="col-span-2 2xl:col-span-3 h-[600px] max-h-[600px] overflow-y-auto" />
+                <CodeMirrorEditor className="col-span-2 2xl:col-span-3 h-[700px] max-h-[700px] overflow-y-auto" />
                 <PreviewSlides />
             </div>
         </div>
