ce99d493ec3e620536d85ee4374431a56c79494d
Simon Schuhmacher <91273944+simonschuhmacher@users.noreply.github.com>
2023-05-16 11:01:38 +0200

    change arg1, arg2 to real parameter names

--- src-main/helpers/ipc.ts
+++ src-main/helpers/ipc.ts
@@ -19,20 +19,20 @@ import {
 } from '../presentation/preview'
 
 export function registerIpc() {
-    ipcMain.handle('dialog:selectFile', (_, arg1, arg2) => selectFile(arg1, arg2))
-    ipcMain.handle('dialog:selectFolder', (_, arg1) => selectFolder(arg1))
-    ipcMain.handle('dialog:selectOutputFile', (_, arg1, arg2) => selectOutputFile(arg1, arg2))
-    ipcMain.handle('dialog:selectOutputFolder', (_, arg1) => selectOutputFolder(arg1))
+    ipcMain.handle('dialog:selectFile', (_, title, filters) => selectFile(title, filters))
+    ipcMain.handle('dialog:selectFolder', (_, title) => selectFolder(title))
+    ipcMain.handle('dialog:selectOutputFile', (_, title, filters) => selectOutputFile(title, filters))
+    ipcMain.handle('dialog:selectOutputFolder', (_, title) => selectOutputFolder(title))
     ipcMain.handle('dialog:saveChanges', saveChangesDialog)
-    ipcMain.handle('file:save', (_, arg1, arg2) => saveFile(arg1, arg2))
-    ipcMain.handle('file:getContent', (_, arg1) => getFileContent(arg1))
-    ipcMain.handle('presentation:parse', (_, arg1) => parse(arg1))
+    ipcMain.handle('file:save', (_, filePath, content) => saveFile(filePath, content))
+    ipcMain.handle('file:getContent', (_, filePath) => getFileContent(filePath))
+    ipcMain.handle('presentation:parse', (_, parseRequest) => parse(parseRequest))
     ipcMain.handle('preview:clearOutFolder', clearPreviewFolder)
-    ipcMain.handle('preview:prepare', (_, arg1) => preparePresentationForPreview(arg1))
+    ipcMain.handle('preview:prepare', (_, presentation) => preparePresentationForPreview(presentation))
     ipcMain.handle('preview:show', openPreviewWindow)
     ipcMain.handle('preview:reload', reloadPreviewWindow)
-    ipcMain.handle('export:html', (_, arg1) => exportHtml(arg1))
-    ipcMain.handle('export:pdf', (_, arg1) => exportPdf(arg1))
+    ipcMain.handle('export:html', (_, exportRequest) => exportHtml(exportRequest))
+    ipcMain.handle('export:pdf', (_, outputPath) => exportPdf(outputPath))
 }
 
 export function unregisterIpc() {
