f144f9fb0a7ce06b63616fa693c5e347fd242d6f
Timo Siegenthaler <timosiegenthaler@gmail.com>
2023-05-22 20:17:00 +0200

    add grammar for better parsing

--- src/components/CodeMirrorEditor.tsx
+++ src/components/CodeMirrorEditor.tsx
@@ -1,13 +1,48 @@
 import { history, indentWithTab, redo, undo } from '@codemirror/commands'
+import { StreamLanguage, LRLanguage, syntaxHighlighting, HighlightStyle } from '@codemirror/language'
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
+import { tags } from '@lezer/highlight'
+import { LRParser } from '@lezer/lr'
+import { parser as mdParser } from '@lezer/markdown'
+import { parseMixed } from '@lezer/common'
+import { yaml } from '@codemirror/legacy-modes/mode/yaml'
 import { EditorState } from '@codemirror/state'
 import { EditorView, ViewPlugin, keymap } from '@codemirror/view'
 import { useEffect, useRef } from 'react'
 import { useEditorStore } from '../store'
 import { oneDark } from '@codemirror/theme-one-dark'
+import { parser } from '../parser/slidesParser'
 
 export type CodeMirrorEditorProps = {
     className?: string
 }
+
+const myHighlightStyle = HighlightStyle.define([
+    { tag: tags.keyword, class: 'text-gray-700 font-bold' },
+    { tag: tags.heading1, class: 'font-bold text-3xl' },
+    { tag: tags.heading2, class: 'font-bold text-2xl' },
+    { tag: tags.heading3, class: 'font-bold text-xl' },
+    { tag: tags.heading4, class: 'font-bold text-lg' },
+    { tag: tags.heading5, class: 'font-bold' },
+    { tag: tags.heading6, class: 'font-bold' },
+    { tag: tags.url, class: 'text-violet-500' },
+])
+
+const myTheme = EditorView.baseTheme({
+    '&': {
+        fontSize: '12pt',
+    },
+})
+
+const mixedParser = parser.configure({
+    wrap: parseMixed(node => {
+        if (node.name === 'YamlContent') return { parser: StreamLanguage.define(yaml).parser }
+        else if (node.name === 'MarkdownBlock') return { parser: mdParser }
+        return null
+    }),
+})
+
+const mixedPlugin = LRLanguage.define({ parser: mixedParser })
 
 export function CodeMirrorEditor(props?: CodeMirrorEditorProps) {
     const editingFilePath = useEditorStore(state => state.editingFilePath)
@@ -16,20 +51,20 @@ export function CodeMirrorEditor(props?: CodeMirrorEditorProps) {
     const editorDomNode = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
-        const myTheme = EditorView.theme({
-            '&': {
-                fontSize: '20px',
-                minHeight: '100%',
-            },
-        })
-
         const updatePlugin = ViewPlugin.define(() => ({
             update: viewUpdate => updateContent(viewUpdate.state.doc.sliceString(0)),
         }))
 
         const state = EditorState.create({
             doc: content,
-            extensions: [oneDark, myTheme, updatePlugin, keymap.of([indentWithTab]), history()],
+            extensions: [
+                myTheme,
+                mixedPlugin,
+                syntaxHighlighting(myHighlightStyle),
+                updatePlugin,
+                keymap.of([indentWithTab]),
+                history(),
+            ],
         })
 
         const parent = editorDomNode.current
