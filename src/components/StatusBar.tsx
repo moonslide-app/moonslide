b8f28368b0148ea28887ff58dfcd066215264cd2
Simon Schuhmacher <91273944+simonschuhmacher@users.noreply.github.com>
2023-06-09 03:35:16 +0200

    status bar update

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -5,7 +5,7 @@ export type StatusBarProps = {
 export function StatusBar(props: StatusBarProps) {
     return (
         <div className="bg-background-secondary text-foreground-primary text-xs border-t border-background-tertiary">
-            <div className="flex justify-between items-baseline px-3 py-1">
+            <div className="flex justify-between items-center px-3 py-1">
                 <p>{`${props.leadingText ?? ''}`}</p>
             </div>
         </div>
